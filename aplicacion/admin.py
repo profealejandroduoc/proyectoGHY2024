984fd6ff2336e0f9a685fb78bfff6989ef5e2037
El profe <al.sepulvedam@profesor.duoc.cl>
2024-10-07 09:50:55 -0300

    Cambios en rama

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artista
 
 # Register your models here.
+class AdmArtistas(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Artista)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Artista(models.Model):
+   
+    nombre=models.CharField(max_length=50,null=False, primary_key=True)
