f41ef376b511cbd14e9c0a0e59802f51c6160380
El profe <al.sepulvedam@profesor.duoc.cl>
2024-10-07 10:20:20 -0300

    Pagina artistas wena

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,artistas
 
 urlpatterns = [
     path('',index, name="index"),
+    path('artistas',artistas, name="artistas"),
 ]
