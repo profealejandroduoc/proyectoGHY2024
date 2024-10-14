a4de6ab202d93f50ad30796246c1a8d1ed25e1e0
CETECOM <cetecom_conce@duoc.cl>
2024-10-14 09:21:44 -0300

    imagen agregada

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Artista(models.Model):
+    nombre=models.CharField(max_length=50, primary_key=True)
+    pais=models.CharField(max_length=25, null=False)
